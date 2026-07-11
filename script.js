5e714bcd84f1374e0dcc7887c4e163ff3de2b910
Miguel Angel <rmiguelangel222@gmail.com>
2026-07-11 11:34:20 +0200

    Solucion de tachado

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 const addBtn = document.getElementById('add-btn');
 const ul = document.querySelector('ul');
+const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+
+
+
+for(const checkbox of checkboxes) {
+    checkbox.addEventListener('change', () => {
+        const li = checkbox.parentElement;
+        const textarea = li.querySelector('input[type="text"]');
+        if(checkbox.checked){
+            textarea.style.textDecoration = 'line-through';
+        }
+        else{
+           textarea.style.textDecoration = 'none'; 
+        }
+    });
+}
 
 addBtn.addEventListener('click', () => {
     crearTarea();
@@ -9,6 +25,5 @@ addBtn.addEventListener('click', () => {
 function crearTarea(){
     const li = document.createElement('li');
     li.innerHTML = '<input type="checkbox"> <input type="text" placeholder="Add a new task">';
-       ul.appendChild(li);
+    ul.appendChild(li);
 }
-
