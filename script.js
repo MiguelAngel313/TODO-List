62730281d52c879fb1c2c43e7f67c3620c2eff64
Miguel Angel <rmiguelangel222@gmail.com>
2026-07-14 20:09:42 +0200

    Ajustar salto de linea de los parrafos

--- script.js
+++ script.js
@@ -1,30 +1,28 @@
-const addBtn = document.getElementById('add-btn');
 const ul = document.querySelector('ul');
-
-document.addEventListener('keydown', (e) => {
-    if(e.key === 'Enter'){
-        crearTarea();
-    }
-});
-
-ul.addEventListener('change', (e) => {
-    if(e.target.matches('input[type="checkbox"]')){
-        const checkbox = e.target;
-        const li = checkbox.parentElement;
-        const textarea = li.querySelector('input[type="text"]')
-
-        checkbox.checked ? textarea.style.textDecoration = 'line-through' : textarea.style.textDecoration = 'none';
-    }
-
-});
+const addBtn = document.getElementById('add-btn');
+const texto = document.querySelector('input');
+let tareas = [];
 
 addBtn.addEventListener('click', () => {
-    crearTarea();
-});
+    crearTarea();    
+})
 
 
 function crearTarea(){
     const li = document.createElement('li');
-    li.innerHTML = '<input type="checkbox"> <input type="text" placeholder="Add a new task">';
-    ul.appendChild(li);
+    const p = document.createElement('p');
+    const checkbox = document.createElement('input');
+    const img = document.createElement('img');
+    img.src = 'trash.svg'
+    checkbox.type = 'checkbox';
+
+    if(texto.value){
+        p.textContent = texto.value;
+        ul.appendChild(li);
+        li.appendChild(checkbox);
+        li.appendChild(p);
+        li.appendChild(img);
+        tareas.push(texto.value);
+        texto.value = '';
+    }
 }
